cac07a9a366c3c3154fd6b382e449596899f1bdb
sanchit02102004 <sanchit.20223229@mnnit.ac.in>
2024-05-23 11:38:11 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+const navIcons = [
+    {src: '/assets/icons/search.svg',alt:'search'},
+    {src: '/assets/icons/black-heart.svg',alt : 'search'},
+    { src: '/assets/icons/search.svg',alt: 'search'},
+]
+
+const Navbar = () => {
+  return (
+    <header className='w-full'>
+        <nav className='nav'>
+          <Link href="/" className="flex items-center gap-1">
+           
+            <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+            />
+            <p className='nav-logo'>
+                Price<span className='text-primary'>Wise</span>
+
+            </p>
+
+
+          
+          </Link>
+          <div className='flex items-center gap5'>
+            {navIcons.map((icon)=>(
+                <Image 
+                key={icon.alt}
+                src={icon.src}
+                alt={icon.alt}
+                width={28}
+                height={28}
+                className='object-contain'
+                />
+               
+            ))}
+          </div>
+          
+
+        </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
